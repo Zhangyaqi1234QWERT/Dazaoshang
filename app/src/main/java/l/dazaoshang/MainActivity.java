35c9ad34fa67c5e32ce7a6c616a6ab8e4b53c533
Zhangyaqi1234QWERT <https://github.com/Zhangyaqi1234QWERT/MyApplication2.git>
2018-05-10 10:33:30 +0800

    一个大晚上

--- app/src/main/java/l/dazaoshang/MainActivity.java
+++ app/src/main/java/l/dazaoshang/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //一个大早上
+        //一个大晚上
     }
 }
